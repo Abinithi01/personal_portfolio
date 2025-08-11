eea595e7d3bcc5f7f71db487ee5ea977f6d048c8
Abinithi01 <abinithig@gmail.com>
2025-08-11 14:26:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: [
+      "Web Developer . . .",
+      "Full Stack Developer...",
+      "UI/UX Interface Designer...",
+      "Frontend Developer . . ." ,
+      "Programmer . . ." , 
+    ],
+    typeSpeed: 60,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
